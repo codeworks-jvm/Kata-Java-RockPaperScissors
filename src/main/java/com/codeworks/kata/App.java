6759bf62d41ff94bf7c57da0c3f52b199593d148
PierrickBottin <bottin.pierrick@gmail.com>
2020-10-11 20:27:55 +0200

    improve readability

--- src/main/java/com/codeworks/kata/App.java
+++ src/main/java/com/codeworks/kata/App.java
@@ -1,9 +1,5 @@
 package com.codeworks.kata;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static String rockPaperScissors(String sign1, String sign2) {
--- src/test/java/com/codeworks/kata/AppTest.java
+++ src/test/java/com/codeworks/kata/AppTest.java
@@ -1,22 +1,15 @@
 package com.codeworks.kata;
 
-
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import java.util.Date;
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AppTest {
-    final Date start = new Date();
 
     @Test
     public void test() {
         // TODO : implement your first test here
     }
-
 }
-
-
